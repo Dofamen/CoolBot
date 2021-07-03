cf5b36322b6149fdc24477558ed69f73c03fc824
El Mehdi El ouakifi <elouakifi.mehdi@gmail.com>
2021-07-03 22:14:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+import os
+from replit import db
+import random
+
+token = os.environ['TOKEN']
+client = discord.Client()
+
+def learn(message):
+  if 'words' not in db.keys():
+    db['words'] = ['nod jma3 karak a wald l9a7ba']
+    return db['words']
+  else:
+    words = db['words']
+    if message not in words:
+      words.append(message)
+      db['words'] = words
+    return words
+
+
+def get_random_word(message):
+  words = learn(message.replace('$', ''))
+  return random.choice(words)
+
+
+@client.event
+async def on_ready():
+  print(f'we have logged as {client.user}')
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user :
+    return 
+  msg = message.content
+  if msg.startswith('$hello'):
+    await message.channel.send('Hello!')
+  elif msg.startswith('$'):
+    m = get_random_word(msg)
+    await message.channel.send(m)
+
+
+
+client.run(token)
